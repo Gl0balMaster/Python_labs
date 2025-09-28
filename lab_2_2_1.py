29b5e27c9f45899e8f84f1997edeeecc93f6ca91
Gl0balMaster <senokos240@gmail.com>
2025-09-28 20:03:39 +0300

    добавлено lab_2_2_1

---
+++ lab_2_2_1.py
@@ -0,0 +1,16 @@
+def flatten_list(items):
+    for k in range(0,len(items)):
+        if(type(items[0]) == list):
+            for i in range(0,len(items[0])):
+                items.append(items[0][i])
+        else:
+            items.append(items[0])
+        items.pop(0)
+        
+    for item in items:
+        if(type(item) != int):
+            flatten_list(items)
+
+
+flatten_list(list_a)  
+print(list_a)
